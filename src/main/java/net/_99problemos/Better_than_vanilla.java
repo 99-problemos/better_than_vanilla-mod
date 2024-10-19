f39e348621a6e3fffe22c5b92b2f4dec411c4c0b
_99problemos <esblair2000@gmail.com>
2024-10-19 13:34:34 +0200

    Adding the Ruby and unrefined ruby blocks.

--- src/main/java/net/_99problemos/Better_than_vanilla.java
+++ src/main/java/net/_99problemos/Better_than_vanilla.java
@@ -1,26 +1,18 @@
 package net._99problemos;
 
-import net._99problemos.ModItems.ModItemGroups;
-import net._99problemos.ModItems.ModItems;
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Better_than_vanilla implements ModInitializer {
-	
 	public static final String MOD_ID = "better_than_vanilla";
-	
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		net._99problemos.ModItemGroups.registerItemGroups();
 
-		/* Calls this function to register mod item groups in the onInit */
-		ModItemGroups.registerItemGroups();
-
-        /* Calls this function to register mod items in the onInit */
 		ModItems.registerModItems();
-
+		net._99problemos.ModBlocks.registerModBlocks();
 	}
 }
--- src/main/java/net/_99problemos/ModItems.java
+++ src/main/java/net/_99problemos/ModItems.java
@@ -1,6 +1,4 @@
-package net._99problemos.ModItems;
-
-import net._99problemos.Better_than_vanilla;
+package net._99problemos;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -11,36 +9,21 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-
-
-
-    /* Makes a new item called the Ruby as in the gemstone different abilities than diamonds does not have the exact functionality as a diamond in game */
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
     public static final Item UNREFINED_RUBY = registerItem("unrefined_ruby", new Item(new FabricItemSettings()));
 
-    /* adds the item to the creative ingredients tab */
-    private static void  addItemsToCreativeIngredientsTabItemGroup(FabricItemGroupEntries entries) {
-
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
         entries.add(UNREFINED_RUBY);
-
     }
 
     private static Item registerItem(String name, Item item) {
-
-        /* Registers the items in the mod */
         return Registry.register(Registries.ITEM, new Identifier(Better_than_vanilla.MOD_ID, name), item);
-
     }
 
     public static void registerModItems() {
+        Better_than_vanilla.LOGGER.info("Registering Mod Items for " + Better_than_vanilla.MOD_ID);
 
-        /* Logger for telling the console that the mod is registering the items. */
-        Better_than_vanilla.LOGGER.info("Registering Mod Items For " + Better_than_vanilla.MOD_ID);
-
-        /* Modifies the creative tab to allow for the Ruby Item to show in the ingredients tab  */
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToCreativeIngredientsTabItemGroup);
-
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
     }
-
 }
